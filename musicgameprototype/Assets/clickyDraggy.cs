7397912c4c97015351297bda191336ba8fe1141d
Dylan <samson559@gmail.com>
2015-07-13 10:14:30 -0500

    7-12
    7-12

---
+++ musicgameprototype/Assets/clickyDraggy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickyDraggy : MonoBehaviour {
+	[SerializeField] private float mouseTolerance = .1f;
+	private Transform lastTransform = null;
+	private Ray ray;
+	private RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray,out hit))
+			{
+				transform.position = new Vector3(hit.point.x,hit.point.y,transform.position.z);
+			}
+		}
+	}
+
+}
