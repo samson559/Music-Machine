277ac7e2da45a880e7a988ab51f3da8aed830dac
Nate Kemp <n.kemp@utah.edu>
2015-07-14 15:57:24 -0600

    Stuff done during session Tuesday

---
+++ musicgameprototype/Assets/LevelData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class songData {
+	public enum Notes {
+		A,
+		C,
+		D
+	}
+	public Notes songNotes = Notes.A;
+	public float noteTime = 0;
+	public float bufferTime = 0;
+}
+
+
+
+public class LevelData : MonoBehaviour {
+
+	public songData[] song;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<Material>().color = new Color (0.5f, 0.1f, 1f, 1f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+}
