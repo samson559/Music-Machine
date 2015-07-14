78b4c346509a7014283a13f5875a553f1335e1d0
Doyle <u0450023@kcmac-32.scl.utah.edu>
2015-07-14 16:54:40 -0600

    7-14
    End of class. Lots of stuff.

--- musicgameprototype/Assets/MarbleStuff.cs
+++ musicgameprototype/Assets/MarbleStuff.cs
@@ -13,23 +13,21 @@ public class MarbleStuff : MonoBehaviour {
 	void Update () {
 	
 	}
-	void Restart()
+	public void Restart()
 	{
 		//put ball back at origin
-		transform.FindChild ("Ball").position = Vector3.zero;
+		transform.FindChild ("Ball").position = transform.position;
 	}
 	// grabbed from http://answers.unity3d.com/questions/171492/how-to-make-a-pause-menu-in-c.html
-	bool togglePause()
+	public void togglePause(string thing)
 	{
 		if(Time.timeScale == 0f)
 		{
 			Time.timeScale = 1f;
-			return(false);
 		}
 		else
 		{
-			Time.timeScale = 0f;
-			return(true);    
+			Time.timeScale = 0f;    
 		}
 	}
 	//end grab.
