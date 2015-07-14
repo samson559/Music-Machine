59ac5ffdefb6d192874e224c5c8f2546eadbd14a
Andrew Roberts <sombreroman55@gmail.com>
2015-07-14 03:12:32 -0600

    Bounce script
    A script to add more upward force to the ball on collision since it
    currently just rolls on blocks.

---
+++ musicgameprototype/Assets/code/Bounce.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+
+    public float force = 300;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        col.collider.attachedRigidbody.AddForce(Vector2.up * force);
+    }
+}
