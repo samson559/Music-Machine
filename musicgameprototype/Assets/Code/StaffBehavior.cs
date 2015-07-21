da67051ffa8c93ab162102d925635050e6e3081c
Nathaniel Ellingson <u0283769@1110mac-3.lib.utah.edu>
2015-07-21 16:18:57 -0600

    lots of staff work

--- musicgameprototype/Assets/Code/StaffBehavior.cs
+++ musicgameprototype/Assets/Code/StaffBehavior.cs
@@ -12,26 +12,44 @@ public class StaffBehavior : MonoBehaviour {
 	[SerializeField] public int measuresDisplayed; // how many measures dispalyed on the screen
 	[SerializeField] public int timesig_top; // top time signature number (the "3" in "3/4" time)
 	[SerializeField] public int timesig_bottom; // bottom time signature number (the "4" in "3/4" time)
-	[SerializeField] public Canvas canvas; // playhead object
+	[SerializeField] public Canvas canvas; // canvas object (from HUD)
 	[SerializeField] public GameObject playhead; // playhead object
-	[SerializeField] public GameObject staff; // playhead object
+	[SerializeField] public GameObject staff; // staff object
 	[SerializeField] public Image timesig_top_image; // top time signature number (the "3" in "3/4" time)
 	[SerializeField] public Image timesig_bottom_image; // bottom time signature number (the "4" in "3/4" time)
 	[SerializeField] public Sprite sprite_3; // top time signature number (the "3" in "3/4" time)
 	[SerializeField] public Sprite sprite_4; // bottom time signature number (the "4" in "3/4" time)
+	[SerializeField] public TextAsset composition;
+
+	private int counter;
 
 	public float speed; // speed at which the playhead moves
 	public bool playing; // is the staff playing music?
 	public float playheadOffset, playheadLimit; // offset from screen center, make room for time signature, clef
-	private Vector3 playheadPosit;
+
+	private Note[] noteArray;
+
+	//private Vector3 playheadPosit;
+
+	private float minX, maxX, rightLimit;
 
 	// Use this for initialization
 	void Start () {
-		//playheadOffset = -200; // test value
-		//x: -366.9
-		//playheadPosit = new Vector3 (playheadOffset, 40,  0);
 		playing = true;
+
+		counter = 0;
+		
+		RectTransform staffTransform = staff.GetComponent<RectTransform>() as RectTransform;
+		RectTransform phTransform = playhead.GetComponent<RectTransform>() as RectTransform;
+
+		rightLimit = 150 * canvas.scaleFactor; // test value
+
+		minX = phTransform.position.x;
+		maxX = minX + (staffTransform.rect.width * canvas.scaleFactor) - rightLimit;
+
 		speed = 0f;
+
+		loadComposition ();
 	}
 	
 	// Update is called once per frame
@@ -75,16 +93,25 @@ public class StaffBehavior : MonoBehaviour {
 
 			// calculate speed
 			float bpm = ((MetronomeBehavior)metronome.GetComponent<MetronomeBehavior> ()).getBPM ();
-			float playheadRange = (staffTransform.rect.width * canvas.scaleFactor) - playheadOffset;
-			float scrollScreenTime = (60 * measuresDisplayed * timesig_bottom) / bpm;
+			float playheadRange = maxX - minX;
+			float scrollScreenTime = (60 * measuresDisplayed * timesig_top) / bpm;
 			speed = playheadRange / scrollScreenTime;
 
-			Debug.Log(playheadRange);
-			Debug.Log(playheadOrigin);
-			
-			phTransform.position = new Vector3 (playheadOrigin.x + (speed * Time.deltaTime), playheadOrigin.y, playheadOrigin.z);
+			if(phTransform.position.x >= maxX) phTransform.position = new Vector3 (minX, playheadOrigin.y, playheadOrigin.z);
+			else phTransform.position = new Vector3 (playheadOrigin.x + (speed * Time.deltaTime), playheadOrigin.y, playheadOrigin.z);
 		}
 
+	}
+
+	public void loadComposition() {
+
+		string[] lines = composition.ToString().Split('\n');
+		noteArray = new Note[lines.Length];
+
+		for(int i = 0; i < noteArray.Length; i++) {
+			string[] values = lines[i].Split(',');
+			noteArray[i] = new Note(values[0], float.Parse(values[1].ToString()));
+		}
 	}
 
 	public void setPlaying(bool play) {
@@ -94,4 +121,51 @@ public class StaffBehavior : MonoBehaviour {
 	public bool isPlaying() {
 		return playing;
 	}
+}
+
+
+class Note {
+	
+	private string name;
+	private float duration;
+	private AudioClip clip;
+	private float beatsPlayed;
+	
+	public Note(string name, float d) {
+		this.name = name;
+		this.duration = d;
+		beatsPlayed = 0;
+		
+		switch(name) {
+		case "C3":
+			clip = Resources.Load("SFX/Piano/C3.wav") as AudioClip;
+			break;
+		case "D3":
+			clip = Resources.Load("SFX/Piano/D3.wav") as AudioClip;
+			break;
+		case "E3":
+			clip = Resources.Load("SFX/Piano/E3.wav") as AudioClip;
+			break;
+		case "F3":
+			clip = Resources.Load("SFX/Piano/F3.wav") as AudioClip;
+			break;
+		case "G3":
+			clip = Resources.Load("SFX/Piano/G3.wav") as AudioClip;
+			break;
+		case "A3":
+			clip = Resources.Load("SFX/Piano/A3.wav") as AudioClip;
+			break;
+		case "B3":
+			clip = Resources.Load("SFX/Piano/B3.wav") as AudioClip;
+			break;
+		case "C4":
+			clip = Resources.Load("SFX/Piano/C4.wav") as AudioClip;
+			break;
+		}
+	}
+	
+	public AudioClip getAudioClip() {
+		return clip;
+	}
+	
 }
--- musicgameprototype/Assets/code/PlayerInput.cs
+++ musicgameprototype/Assets/code/PlayerInput.cs
@@ -5,6 +5,7 @@ public class PlayerInput : MonoBehaviour {
 
 	public float dragSpeed = 0.5f;
 	private Vector3 dragOrigin;
+	private float minX, minY, maxX, maxY; // min and max X and Y position for camera, based on size of stage
 
 	/*
 	 * This class handles player input, such as pressing the pause button to pause playback, pressing the reset button to reset the level, etc.
@@ -13,6 +14,14 @@ public class PlayerInput : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		float stageWidth = (gameObject.GetComponent<StageData> () as StageData).getStageWidth();
+		float stageHeight = (gameObject.GetComponent<StageData> () as StageData).getStageHeight();
+		
+		minX = (gameObject.GetComponent<Transform> () as Transform).position.x - (stageWidth / 2);
+		minY = (gameObject.GetComponent<Transform> () as Transform).position.y - (stageHeight / 2);
+		maxX = minX + stageWidth;
+		maxY = minY + stageHeight;
+
 		Time.timeScale = 0; // start the simualtion paused.
 	}
 	
@@ -20,6 +29,13 @@ public class PlayerInput : MonoBehaviour {
 	void Update () {
 
 		if(Input.GetButton ("panOption")) mousePan ();
+		
+		if (Input.GetButton ("reset")) {
+			MarbleSpawnBehavior[] msb = FindObjectsOfType<MarbleSpawnBehavior>() as MarbleSpawnBehavior[];
+			for(int i = 0; i < msb.Length; i++) {
+				msb[i].setComplete(false);
+			}
+		}
 
 		if (Input.GetButtonDown ("Pause")) {
 			if(Time.timeScale == 0) Time.timeScale = 1; // restart the simulation;
@@ -29,31 +45,35 @@ public class PlayerInput : MonoBehaviour {
 	}
 
 	void mousePan() {
-		if (Input.GetMouseButtonDown(0))
-		{
+		if (Input.GetMouseButtonDown(0)) {
 			dragOrigin = Input.mousePosition;
 			return;
-		}
-
-		if (Input.GetButton ("reset")) {
-			MarbleSpawnBehavior[] msb = FindObjectsOfType<MarbleSpawnBehavior>() as MarbleSpawnBehavior[];
-			for(int i = 0; i < msb.Length; i++) {
-				msb[i].setComplete(false);
-			}
-			
-			//Destroy(GameObject.FindObjectsOfType<MarbleBehavior>());
-
-			/*
-			MarbleSpawnBehavior[] mb = FindObjectsOfType<MarbleBehavior>() as MarbleBehavior[];
-			for(int i = 0; i < mb.Length; i++) {
-			}*/
 		}
 		
 		if (!Input.GetMouseButton(0)) return;
 		
-		Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-		Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+		Vector3 cameraPos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+		Vector3 cameraMove = new Vector3(cameraPos.x * dragSpeed, cameraPos.y * dragSpeed, 0);
+
+		Transform cameraT = gameObject.GetComponent<Transform> () as Transform;
 		
-		transform.Translate(move, Space.World);  
+		cameraT.Translate (cameraMove);
+
+		// constrain camera to stage size
+		float newX = cameraT.position.x;
+		float newY = cameraT.position.y;
+		if (cameraT.position.x < minX)
+			newX = minX;
+		else if (cameraT.position.x > maxX)
+			newX = maxX;
+		if (cameraT.position.y < minY)
+			newY = minY;
+		else if (cameraT.position.y > maxY)
+			newY = maxY;
+
+		cameraT.position = new Vector3 (newX, newY, cameraT.position.z);
+
+
+		Debug.Log (cameraT.position);  
 	}
 }
