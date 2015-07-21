31ae02ff8a00c61346b37abfa24c73765048c230
Dylan <samson559@gmail.com>
2015-07-21 01:43:00 -0500

    Instantiation
    Literally two lines of code to add to a button to make it spawn music
    objects on click.

---
+++ musicgameprototype/Assets/musicObjMaker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicObjMaker : MonoBehaviour {
+	[SerializeField] private GameObject note;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void makeNote()
+	{
+		GameObject go = Instantiate<GameObject> (note);
+	}
+
+}
