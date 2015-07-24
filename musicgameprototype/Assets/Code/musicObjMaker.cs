4b37108dc9b8470c97c1919a7501ceb36a051aee
Nate Kemp <n.kemp@utah.edu>
2015-07-23 19:57:06 -0600

    Reconfigured Note spawning and other stuff.
    Notes spawn in middle of screen. Dylan did this.

--- musicgameprototype/Assets/Code/musicObjMaker.cs
+++ musicgameprototype/Assets/Code/musicObjMaker.cs
@@ -4,7 +4,7 @@ using UnityEngine.EventSystems;
 
 using System.Collections;
 
-public class musicObjMaker : MonoBehaviour, IPointerDownHandler {
+public class musicObjMaker : MonoBehaviour {
 	[SerializeField] private GameObject note;
 
 	private Button button;
@@ -18,28 +18,31 @@ public class musicObjMaker : MonoBehaviour, IPointerDownHandler {
 
 	}
 
-	public void OnPointerDown(PointerEventData data) {
-		makeNote ();
-	}
+//	public void OnPointerDown(PointerEventData data) {
+//		makeNote ();
+//	}
 
 	public void makeNote() {
-		Transform noteT = note.GetComponent<Transform> ();
-
-		// create plane on which to place note
-		Plane gamePlane = new Plane ();
-		Vector3 planePoint1 = new Vector3 (0, 0, noteT.position.z); // noteT already has the correct Z coord, we need to find the right x and y coords...
-		Vector3 planePoint2 = new Vector3 (0, 1, noteT.position.z);
-		Vector3 planePoint3 = new Vector3 (1, 1, noteT.position.z);
-		gamePlane.Set3Points (planePoint1, planePoint2, planePoint3);
-
-		// calculate intersection of gamePlane with a ray going from the camera to the mouse position
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		float dist; // dist must first be declared
-		Vector3 where = Vector3.zero; // initialize where to (0,0,0)
-		if (gamePlane.Raycast (ray, out dist)) // I am not familiar with C# syntax, but this works...
-			where = ray.GetPoint (dist);
-
-		// instantiate object
+//		Transform noteT = note.GetComponent<Transform> ();
+//
+//		// create plane on which to place note
+//		Plane gamePlane = new Plane ();
+//		Vector3 planePoint1 = new Vector3 (0, 0, noteT.position.z); // noteT already has the correct Z coord, we need to find the right x and y coords...
+//		Vector3 planePoint2 = new Vector3 (0, 1, noteT.position.z);
+//		Vector3 planePoint3 = new Vector3 (1, 1, noteT.position.z);
+//		gamePlane.Set3Points (planePoint1, planePoint2, planePoint3);
+//
+//		// calculate intersection of gamePlane with a ray going from the camera to the mouse position
+//		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//		float dist; // dist must first be declared
+//		Vector3 where = Vector3.zero; // initialize where to (0,0,0)
+//		if (gamePlane.Raycast (ray, out dist)) // I am not familiar with C# syntax, but this works...
+//			where = ray.GetPoint (dist);
+//
+//		// instantiate object
+//		Object go = Instantiate(note, where, Quaternion.identity);
+//		Nate, we're going to try this for awhile
+		Vector3 where = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, 56.13f);
 		Object go = Instantiate(note, where, Quaternion.identity);
 	}
 
