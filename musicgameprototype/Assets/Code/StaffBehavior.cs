5533a1fed28b7f6829b00aca3b6c15b3b504d7a3
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-07-25 15:08:20 -0600

    Lots of staff updates, notes spawn
    However, I'll need to tweak them to place them exactly. Still, they
    generally spawn close to the right place at the moment.

--- musicgameprototype/Assets/Code/StaffBehavior.cs
+++ musicgameprototype/Assets/Code/StaffBehavior.cs
@@ -20,15 +20,17 @@ public class StaffBehavior : MonoBehaviour {
 	[SerializeField] public Sprite sprite_3; // top time signature number (the "3" in "3/4" time)
 	[SerializeField] public Sprite sprite_4; // bottom time signature number (the "4" in "3/4" time)
 	[SerializeField] public TextAsset composition;
+	[SerializeField] public GameObject note;
 
 	private int counter;
 
 	public float speed; // speed at which the playhead moves
 	public bool playing, compositionLoaded; // is the staff playing music?
 	public float playheadOffset, playheadLimit; // offset from screen center, make room for time signature, clef
-	private Note[] noteArray; // array of notes which are loaded from txt file
+	private GameObject[] noteArray; // array of notes which are loaded from txt file
 	private int page, totalPages; // current page of composition;
-	private float minX, maxX, rightLimit;
+	private float minX, maxX, rightLimit, leftLimit;
+	private RectTransform staffTransform, phTransform, tstopTransform, tbottomTransform;
 
 	// Use this for initialization
 	void Start () {
@@ -38,14 +40,26 @@ public class StaffBehavior : MonoBehaviour {
 		page = 0;
 		totalPages = 0;
 		compositionLoaded = false;
+
+		staffTransform = staff.GetComponent<RectTransform>();
+		phTransform = playhead.GetComponent<RectTransform>();
+		tstopTransform = timesig_top_image.GetComponent<RectTransform> ();
+		tbottomTransform = timesig_bottom_image.GetComponent<RectTransform>();
+
+		staffTransform.parent = GameObject.FindGameObjectWithTag ("HUD").GetComponent<RectTransform> ();
+		phTransform.parent = staffTransform;
+		tstopTransform.parent = staffTransform;
+		tbottomTransform.parent = staffTransform;
+
+		leftLimit = 100;
+		rightLimit = 100;
 		
-		RectTransform staffTransform = staff.GetComponent<RectTransform>() as RectTransform;
-		RectTransform phTransform = playhead.GetComponent<RectTransform>() as RectTransform;
+		minX = 100;
+		maxX = (staffTransform.rect.width * canvas.scaleFactor) - rightLimit;
 
-		rightLimit = 400 * canvas.scaleFactor; // test value
-
-		minX = phTransform.position.x;
-		maxX = minX + (staffTransform.rect.width * canvas.scaleFactor) - rightLimit;
+		phTransform.position = new Vector3 (minX, phTransform.position.y, phTransform.position.z);
+		tstopTransform.position = new Vector3 (minX - 40, tstopTransform.position.y, tstopTransform.position.z);
+		tbottomTransform.position = new Vector3 (minX - 40, tbottomTransform.position.y, tbottomTransform.position.z);
 
 		speed = 0f;
 
@@ -88,7 +102,7 @@ public class StaffBehavior : MonoBehaviour {
 		
 		speed = 0f;
 		
-		if (playing && compositionLoaded) {
+		if (playing && compositionLoaded) { // only scroll
 			GameObject metronome = GameObject.Find ("Metronome");
 			
 			// calculate speed
@@ -96,7 +110,7 @@ public class StaffBehavior : MonoBehaviour {
 			float playheadRange = maxX - minX;
 			//float distPerBeat = playheadRange / (timesig_top * measuresDisplayed);
 			//float distToNextBeat = (Mathf.Floor(phTransform.position.x/playheadRange) * distPerBeat);
-			float scrollScreenTime = (60 * measuresDisplayed * timesig_top) / bpm;
+			float scrollScreenTime = (60 * measuresDisplayed * timesig_top) / bpm; // the 60 converts from beats per minute to beats per second
 			speed = playheadRange / scrollScreenTime;
 			//speed = distToNextBeat/metronome.GetComponent<MetronomeBehavior>().getSecToNextBeat();
 
@@ -105,14 +119,68 @@ public class StaffBehavior : MonoBehaviour {
 		}
 		
 	}
+
 	public void loadComposition() {
 
 		string[] lines = composition.ToString().Split('\n');
-		noteArray = new Note[lines.Length];
+		noteArray = new GameObject[lines.Length];
+		float totalBeats = 0;
+		int currentPage = 1;
 
+		// load each note
 		for(int i = 0; i < noteArray.Length; i++) {
 			string[] values = lines[i].Split(',');
-			noteArray[i] = new Note(values[0], int.Parse(values[1].ToString()));
+			noteArray[i] = Instantiate(note, Vector3.zero, Quaternion.identity) as GameObject;
+			noteArray[i].transform.parent = staff.GetComponent<RectTransform>();
+			StaffNoteData snd = noteArray[i].GetComponent<StaffNoteData>();
+			snd.setName(values[0]);
+			snd.setBeatsPlayed(float.Parse(values[1]));
+
+			RectTransform nRTransform = noteArray[i].GetComponent<RectTransform>();
+			Vector3 notePos = nRTransform.position;
+
+			float noteYBase = 6;
+			float noteYSpacing = 9;
+
+			switch(values[0]) {
+			case "C3":
+				notePos.y = noteYBase + 0 * noteYSpacing;
+				break;
+			case "D3":
+				notePos.y = noteYBase + 1 * noteYSpacing;
+				break;
+			case "E3":
+				notePos.y = noteYBase + 2 * noteYSpacing;
+				break;
+			case "F3":
+				notePos.y = noteYBase + 3 * noteYSpacing;
+				break;
+			case "G3":
+				notePos.y = noteYBase + 4 * noteYSpacing;
+				break;
+			case "A3":
+				notePos.y = noteYBase + 5 * noteYSpacing;
+				break;
+			case "B3":
+				notePos.y = noteYBase + 6 * noteYSpacing;
+				break;
+			case "C4":
+				notePos.y = noteYBase + 7 * noteYSpacing;
+				break;
+			}
+
+
+			snd.setPage(currentPage);
+			if((totalBeats / measuresDisplayed * timesig_top) > currentPage) currentPage ++; 
+
+			float playheadRange = maxX - minX;
+			float noteSpacing = (playheadRange / (timesig_top * measuresDisplayed));
+			float noteXOffset = totalBeats * noteSpacing;
+			notePos.x += minX + noteXOffset;
+			totalBeats += snd.getBeatsPlayed();
+
+
+			nRTransform.position = new Vector3(notePos.x, notePos.y, notePos.z);
 		}
 
 		page = 0;
@@ -122,6 +190,7 @@ public class StaffBehavior : MonoBehaviour {
 
 		compositionLoaded = true;
 	}
+
 	public void setPlaying(bool play) {
 		playing = play;
 	}
@@ -138,34 +207,25 @@ public class StaffBehavior : MonoBehaviour {
 		playing = false;
 	}
 
-	private int calcBeatsInCompotion(Note[] array) {
-		int totalBeats = 0;
+	private float calcBeatsInCompotion(GameObject[] array) {
+		float totalBeats = 0;
 
 		for (int i = 0; i < array.Length; i++) {
-			totalBeats += array[i].getBeatsPlayed();
+			StaffNoteData snd = array[i].GetComponent<StaffNoteData>();
+			totalBeats += snd.getBeatsPlayed();
 		}
-
 		return totalBeats;
 	}
-}
 
+	public void resetStaff() {
+		page = 1; // go back to first page
 
-class Note {
-	
-	private string name;
-	private int beatsPlayed;
-	
-	public Note(string name, int bp) {
-		this.name = name;
-		beatsPlayed = bp;
+		// reset playhead position
+		Transform phTransform = playhead.GetComponent<Transform> ();
+		Vector3 playheadOrigin = new Vector3 (phTransform.position.x, phTransform.position.y, phTransform.position.z);
+		phTransform.position = new Vector3 (minX, playheadOrigin.y, playheadOrigin.z);
+
+		// stop tick sound
+		playing = false;
 	}
-
-	public int getBeatsPlayed() {
-		return beatsPlayed;
-	}
-
-	public string getName()  {
-		return name;
-	}
-	
 }
---
+++ musicgameprototype/Assets/Code/StaffNoteData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaffNoteData : MonoBehaviour {
+
+	[SerializeField] public string name;
+	[SerializeField] public float beatsPlayed;
+	[SerializeField] public int page;
+	
+	public StaffNoteData(string name, int bp, int page) {
+		this.name = name;
+		beatsPlayed = bp;
+	}
+
+	public void setPage(int p) {
+		page = p;
+	}
+	
+	public float getBeatsPlayed() {
+		return beatsPlayed;
+	}
+	
+	public string getName()  {
+		return name;
+	}
+
+	public void setBeatsPlayed(float bp) {
+		this.beatsPlayed = bp;
+	}
+
+	public void setName(string n) {
+		this.name = n;
+	}
+}
