2c6f76234e1b3f924fcfa90f7cf1b9ed410db9a7
Dylan <samson559@gmail.com>
2015-07-27 10:09:46 -0500

    Adding main menu
    main menu scene currently loads one scene. Will need to add scenes to
    build in build settings as we make new ones. I'm only half (or less)
    done with my assigned tasks so don't treat any of these as final.

---
+++ musicgameprototype/Assets/Code/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿////mainmenu
+/// Attached to camera
+using UnityEngine;
+using System.Collections;
+public class MainMenu : MonoBehaviour {
+	[SerializeField] private Texture Mainmenutexture;
+	[SerializeField] private Texture Level1Thumb;
+	[SerializeField] private Texture Level2Thumb;
+	[SerializeField] private Texture Level3Thumb;
+
+
+
+
+	void OnGUI(){
+
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), Mainmenutexture);
+	
+///Button display
+		/// Make sure levels are included in build settings and adhere to naming convention "LevelX".
+		/// level1
+		if (GUI.Button (new Rect(Screen.width * .5f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), new GUIContent("Play Level1",Level1Thumb),"")) {
+			Application.LoadLevel(1);
+		}
+		///level2
+		if (GUI.Button (new Rect(Screen.width * .5f, Screen.height * .6f, Screen.width * .5f, Screen.height * .1f), new GUIContent("Play Level2",Level2Thumb),"")) {
+			Application.LoadLevel("Level2");
+		}
+		///level2
+		if (GUI.Button (new Rect(Screen.width * .5f, Screen.height * .7f, Screen.width * .5f, Screen.height * .1f), new GUIContent("Play Level3",Level3Thumb),"")) {
+			Application.LoadLevel("Level3");
+		}
+
+	}
+}
