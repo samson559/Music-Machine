7376281f557a66cd022556d2f7cb2fb6df374e23
Dylan <samson559@gmail.com>
2015-07-28 01:57:05 -0500

    more menu stuff
    Main menu uses a canvas/eventsystem now. much more modular.

---
+++ musicgameprototype/Assets/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void loadLevel(string _leveltoLoad)
+	{
+		Application.LoadLevel (_leveltoLoad);
+	}
+}
