3a99545181961214e2cc0082cab0a28bda24e643
Nate Kemp <n.kemp@utah.edu>
2015-07-28 15:15:20 -0600

    Level Menu Stuff
    Put Star Wars in as a Demo Level

---
+++ musicgameprototype/Assets/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void LoadLevel(string _LevelToLoad)
+	{
+		Application.LoadLevel (_LevelToLoad);
+	}
+}
