67d6c9d504d10590302595db064bb08b61e60e46
Doyle <u0450023@1110mac-5.lib.utah.edu>
2015-08-06 16:50:39 -0600

    ClickyDraggyUpdate
    Click draggy is disabled on play

--- musicgameprototype/Assets/Code/clickyDraggy.cs
+++ musicgameprototype/Assets/Code/clickyDraggy.cs
@@ -11,46 +11,50 @@ public class clickyDraggy : MonoBehaviour {
 	private Vector3 screenPoint;
 	private Vector3 offset;
 	private float prevMouse;
+	private StaffBehavior theStaff;
 	//This is from http://answers.unity3d.com/questions/12322/drag-gameobject-with-mouse.html
 	public void OnMouseDown()
 	{
-		// delete when hold 'deleteOption' key
-		if (Input.GetButton ("deleteOption")) {
-			Destroy (gameObject);
-			return;
+		if (!theStaff.playing) {
+			// delete when hold 'deleteOption' key
+			if (Input.GetButton ("deleteOption")) {
+				Destroy (gameObject);
+				return;
+			}
+			
+			screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+			
+			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 		}
-
-		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-		
-		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 		
 	}
 	
 	void OnMouseDrag()
 	{
-
-		if (Input.GetButton ("rotateOption")) {
-			transform.Rotate (new Vector3 (0, 0, Input.mousePosition.y - prevMouse));
+		if (!theStaff.playing) {
+			if (Input.GetButton ("rotateOption")) {
+				transform.Rotate (new Vector3 (0, 0, Input.mousePosition.y - prevMouse));
+				prevMouse = Input.mousePosition.y;
+				return;
+			}
+			Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			
+			Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+			transform.position = curPosition;
 			prevMouse = Input.mousePosition.y;
-			return;
 		}
-		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		
-		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint)+ offset;
-		transform.position = curPosition;
-		prevMouse = Input.mousePosition.y;
 	}
 	// end citation
 	// Use this for initialization
 	void Start () {
-	
+		theStaff = GameObject.Find ("staff").GetComponent<StaffBehavior> ();
 	}
-
+	
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
-
-
+	
+	
 }
