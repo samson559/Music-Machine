36da9cd80f7d4a96fc34e402a890a4ea2d8b1b10
Dylan <samson559@gmail.com>
2015-08-06 14:40:42 -0500

    moving cannon
    It moves. so pretty. might want to set bobrate to zero so there is
    consistent launch.

---
+++ musicgameprototype/Assets/CannonRotator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonRotator : MonoBehaviour {
+	[SerializeField] private float rotationRate;
+	[SerializeField] private float bob;
+	[SerializeField] private float bobRate;
+	private bool up= true;
+	private float bobTemp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (bobTemp > bob)
+			up = false;
+		else if (bobTemp <= 0)
+			up = true;
+		if (up) {
+			bobTemp += Time.deltaTime * bob;
+			transform.position += Vector3.up * bobRate;
+		} else {
+			bobTemp -= Time.deltaTime * bob;
+			transform.position -= Vector3.up * bobRate;
+		}
+
+
+		transform.Rotate (new Vector3(0,rotationRate,0));
+	}
+}
